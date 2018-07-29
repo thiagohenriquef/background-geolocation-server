dabc466ee661d91e44d379610ac54b9210992aff
Thiago Henrique Ferreira <thiago.ferreira10@hotmail.com>
2018-07-29 02:02:42 -0300

    heroku db

--- db/index.js
+++ db/index.js
@@ -1,15 +1,15 @@
 const { Pool } = require('pg')
-// const host = 'ec2-54-197-230-161.compute-1.amazonaws.com'
-// const database = 'd3j7al81ieuhoh'
-// const user = 'ghoidalghcdjui'
-// const port = '5432'
-// const password = '525b8b7d58344774ba647587a4d8ae06c0d9eb011642dffc1de9f244c3624bad'
+const host = 'ec2-50-16-241-91.compute-1.amazonaws.com'
+const database = 'd82p02b7pbmksv'
+const user = 'lqonsvarkvyktb'
+const port = '5432'
+const password = '630a97c3e56adb0143e9d9d298c6ba8fd9d21c6018d5e2bf1e62d3fd8649ea67'
 
-// const connectionString = `postgresql://${user}:${password}@${host}:${port}/${database}`
-// const pool = new Pool({ connectionString: connectionString, ssl: true })
+const connectionString = `postgresql://${user}:${password}@${host}:${port}/${database}`
+const pool = new Pool({ connectionString: connectionString, ssl: true })
 
-const connectionString = 'postgresql://postgres:postgres@localhost:5432/geolocalizacao'
-const pool = new Pool({ connectionString })
+// const connectionString = 'postgresql://postgres:postgres@localhost:5432/geolocalizacao'
+// const pool = new Pool({ connectionString })
 
 module.exports = {
   query: (text, params, callback) => {
