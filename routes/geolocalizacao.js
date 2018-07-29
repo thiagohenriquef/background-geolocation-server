6308b08347a5876610e09b3f9278da9b69cc58f2
Thiago Henrique Ferreira <thiago.ferreira10@hotmail.com>
2018-07-29 02:24:56 -0300

    rota get

--- routes/geolocalizacao.js
+++ routes/geolocalizacao.js
@@ -2,10 +2,16 @@ const db = require('../db')
 const router = require('express').Router()
 const errorCallback = err => res.status(500).send(err)
 
+router.get('/', (req, res) => {
+  db.query('select * from geolocalizacao')
+    .then(({ rows }) => res.status(200).json(rows))
+    .catch(errorCallback)
+})
+
 router.post('/', (req, res) => {
   console.log('------------------------------------')
   console.log(req.body)
-  // db.query('insert into objetivos(texto, concluido) values ($1, false) RETURNING id', [req.body.texto])
+  // db.query('insert into geolocalizacao(latitude, longitude) values ($1, false) RETURNING id', [req.body.texto])
     // .then(({ rows }) => res.status(200).json(rows[0].id))
     // .catch(errorCallback)
 })
