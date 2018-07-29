cbce3eb24c445dfbe3e7e33ccff00232273eca91
Thiago Henrique Ferreira <thiago.ferreira10@hotmail.com>
2018-07-29 02:32:44 -0300

    enable cors

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const app = require('express')()
 const middleware = require('./utils/middleware')
 const routes = require('./routes/index')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(cors())
 
 // Loading middleware...
 app.use(middleware)
